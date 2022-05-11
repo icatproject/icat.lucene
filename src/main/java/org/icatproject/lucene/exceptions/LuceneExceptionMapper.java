78b0295589c3e98f04cd905a881fe78a102e0fea
Alan Kyffin <alan.kyffin@stfc.ac.uk>
2022-05-11 14:56:59 +0100

    Upgrade to jakartaee-api 9.1.0

--- src/main/java/org/icatproject/lucene/exceptions/LuceneExceptionMapper.java
+++ src/main/java/org/icatproject/lucene/exceptions/LuceneExceptionMapper.java
@@ -2,11 +2,11 @@ package org.icatproject.lucene.exceptions;
 
 import java.io.ByteArrayOutputStream;
 
-import javax.json.Json;
-import javax.json.stream.JsonGenerator;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.ext.ExceptionMapper;
-import javax.ws.rs.ext.Provider;
+import jakarta.json.Json;
+import jakarta.json.stream.JsonGenerator;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
 
 @Provider
 public class LuceneExceptionMapper implements ExceptionMapper<LuceneException> {
